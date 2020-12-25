d5971254e8ad7b84e94f370a24dfb966e493ba3b
hieuitkma <hieuitkma@gmail.com>
2020-12-25 09:00:42 +0700

    add content

--- src/js/app.js
+++ src/js/app.js
@@ -2,33 +2,28 @@ import '../styles/app.scss';
 
 console.log('Starting kmacoders dev !!!');
 
-let dot = document.querySelectorAll(".bar");
-console.log(dot);
-// dot[0].setAttribute("style", "background-color: white;");
+console.log(window);
 
-let checked = document.querySelectorAll('input[name="r"]');
-console.log(checked);
-Array.from(checked).forEach((item, i) => {
-    item.addEventListener('click', () => {
-        dot[i].setAttribute("style", "background-color: white;");
-    })
+//header-top
+let mainNav = document.querySelector('.main-nav');
+document.addEventListener('scroll', function(e) {
+    mainNav.classList.add('class', 'main-nav__sticky');
+});
+
+// tablet icon 
+let plus = document.querySelector('.nav__tablet--item__icon i');
+let minus = document.querySelectorAll('.nav__tablet--item__icon i');
+
+let liTag = document.querySelectorAll('.nav__tablet--item');
+liTag[0].addEventListener('click', () => {
+    liTag[0].classList.toggle("nav__tablet--item-click");
+
+    plus.classList.toggle('rotatePlus');
+
+    minus[1].classList.toggle('rotateMinus')
 })
 
-let i1 = document.querySelector('.nav__tablet--item__icon i');
-let i2 = document.querySelectorAll('.nav__tablet--item__icon i');
-
-let test = document.querySelectorAll('.nav__tablet--item');
-console.log(test[0]);
-test[0].addEventListener('click', () => {
-    test[0].classList.toggle("nav__tablet--item-click");
-
-    i1.classList.toggle('rotatePlus');
-
-    i2[1].classList.toggle('rotateMinus')
-})
-
-
-
+// slide
 var swiper = new Swiper('.swiper-container', {
     spaceBetween: 30,
     centeredSlides: true,
